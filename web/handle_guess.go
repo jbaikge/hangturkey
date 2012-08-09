a8a9f1fc219b3a549b21d508fe0a92e82475a56e
jake <bigjake2s@gmail.com>
2012-08-09 12:06:50 -0400

    Extracting guess letter from URL

--- web/handle_guess.go
+++ web/handle_guess.go
@@ -1,15 +1,25 @@
 package web
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
+const guessURL = "/guess/"
+
 func init() {
-	http.Handle("/guess", WebHandler(GuessHandler))
+	http.Handle(guessURL, WebHandler(GuessHandler))
 }
 
 func GuessHandler(w http.ResponseWriter, req *http.Request, ctx *Context) error {
-	fmt.Fprintf(w, "GUESS: %s", req.URL.String())
+	letter := strings.Replace(req.URL.String(), guessURL, "", 1)
+	if len(letter) == 0 {
+		return errors.New("No letter provided")
+	}
+	letter = letter[0:1]
+	fmt.Fprintf(w, "GUESS:  %s\n", req.URL.String())
+	fmt.Fprintf(w, "LETTER: %s\n", letter)
 	return nil
 }
