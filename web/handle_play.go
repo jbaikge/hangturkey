d986ce4263dd89fc8e6a247080ec933776b37248
jake <bigjake2s@gmail.com>
2012-08-10 07:58:08 -0400

    Temporarily parsing play templates every hit; Setting up structure closer to the final.

--- web/handle_play.go
+++ web/handle_play.go
@@ -4,15 +4,14 @@ import (
 	"net/http"
 )
 
-var play = parseTemplates(
-	"web/templates/_base.html",
-	"web/templates/play.html",
-)
-
 func init() {
 	http.Handle("/play", WebHandler(PlayHandler))
 }
 
 func PlayHandler(w http.ResponseWriter, req *http.Request, ctx *Context) error {
+	var play = parseTemplates(
+		"web/templates/_base.html",
+		"web/templates/play.html",
+	)
 	return play.Execute(w, ctx.State)
 }
