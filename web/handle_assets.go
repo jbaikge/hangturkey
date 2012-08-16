ca3a66c84039112b8763618b104ac6b59b8883ef
jake <bigjake2s@gmail.com>
2012-08-16 10:17:47 -0400

    @zeebo saves the day again - Synology doesn't provide any mime.types files

--- web/handle_assets.go
+++ web/handle_assets.go
@@ -1,9 +1,11 @@
 package web
 
 import (
+	"mime"
 	"net/http"
 )
 
 func init() {
+	mime.AddExtensionType(".svg", "image/svg+xml")
 	http.Handle("/images/", http.FileServer(http.Dir("web/assets")))
 }
